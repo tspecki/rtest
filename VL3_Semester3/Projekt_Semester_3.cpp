058913a994a03f78cd782c77e3a047d0faf62d0e
fgaertner <fegio@web.de>
2017-11-03 11:07:01 +0100

    Suck a dick

--- VL3_Semester3/Projekt_Semester_3.cpp
+++ VL3_Semester3/Projekt_Semester_3.cpp
@@ -1,61 +1,88 @@
-// VL3_Semester3.cpp : Defines the entry point for the console application.
+// Space Spiel
 //
 
 #include "stdafx.h"
 #include <Gosu/Gosu.hpp>
 #include <Gosu/AutoLink.hpp>
-
-const int Fensterbreite = 1081;
-const int Fensterhöhe = 800;
-
-double mausx, mausy;
-double Rotation;
+//using namespace std;
 
 class GameWindow : public Gosu::Window {
 public:
-	Gosu::Image bild;
-	Gosu::Image bild1;
+	Gosu::Image bild, bild2, pic_fire;
 	GameWindow()
-		: Window(Fensterbreite,Fensterhöhe)
-		, bild("Felix.png")
-		, bild1("Scope.png")
+		: Window(1500, 900)
+		, bild("sternenhimmel.png")
+		, bild2("rakete.png")
+		, pic_fire("fire.png")
 	{
-		set_caption("shoot Felix");
+		set_caption("Testspiel");
+	}
+
+	double x = 0.0;
+	double y = 400.0;
+
+	double rot_fire = 0.0;
+
+	double ref_x = 0.0;
+	double ref_y = 0.0;
+
+	double flames(double min, double max)
+	{
+		double f = (double)rand() / RAND_MAX;
+		return min + f*(max - min);
+	}
+
+	void rocket()
+	{
+		bild2.draw_rot(600.0, 650.0, 0.0, 0.0, 0.0, 0.0);
+
+		if (input().down(Gosu::KB_UP))
+		{
+			pic_fire.draw_rot(756.0, 818.0, 0.0, rot_fire, 0.6, 0.0, 0.2, 0.2);
+		}
+	}
+	void background()
+	{
+		double x0 = x;
+		double y0 = y;
+
+		while (x0 > 0)
+		{
+			x0 -= 500.0;
+		}
+
+		for (double x1 = x0; x1 < graphics().width(); x1 += 500.0)
+		{
+			bild.draw_rot(x1, y, 0.0, 0.0, 0.0, 0.0);
+			bild.draw_rot(x1, y - 500.0, 0.0, 0.0, 0.0, 0.0);
+			bild.draw_rot(x1, y - 1000.0, 0.0, 0.0, 0.0, 0.0);
+		}
 	}
 
 	void update() override {
-		mausx = input().mouse_x();
-		mausy = input().mouse_y();
-		Rotation += 1;
+
+		if (input().down(Gosu::KB_LEFT))
+		{
+			x = x + 2;
+			ref_x = ref_x - 1;
+		}
+		else if (input().down(Gosu::KB_RIGHT))
+		{
+			x = x - 2;
+		}
+		if (input().down(Gosu::KB_UP))
+		{
+			y = y + 2;
+			rot_fire = flames(-9.0, 9.0);
+		}
 	}
-
 	void draw() override {
-		
-		/*graphics().draw_line(
-			10, 20, Gosu::Color::RED,
-			200, 100, Gosu::Color::GREEN,
-			0.0
-		);
-		graphics().draw_triangle(100, 100, Gosu::Color::GREEN,
-			250, 150, Gosu::Color::AQUA,
-			150, 300, Gosu::Color::BLUE,
-			0.1
-		);*/
-		bild.draw_rot(Fensterbreite/2,Fensterhöhe/2, 0.0,
-			Rotation, // Rotationswinkel in Grad
-			0.5, 0.5 // Position der "Mitte" relativ zu x, y
-		);
-
-		bild1.draw_rot(mausx, mausy, 0.0,
-			0, // Rotationswinkel in Grad
-			0.5, 0.5 // Position der "Mitte" relativ zu x, y
-		);
+		background();
+		rocket();
 	}
 };
-
-
 int main()
 {
-GameWindow window;
-window.show();
+	GameWindow window;
+	window.show();
 }
